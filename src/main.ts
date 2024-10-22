1aaf8ec766fcf5e0ee305c1c4fa0dc0f6653ad2b
Ana <162636836+actiasAnii@users.noreply.github.com>
2024-10-21 20:19:27 -0700

    step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,35 @@ const APP_NAME = "Sketchpad Demo !";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+// reuse the helper function from my demo 1 to create the title and canvas
+function createElement<K extends keyof HTMLElementTagNameMap>(
+    tag: K,
+    options: {
+      styles?: Partial<CSSStyleDeclaration>;
+      textContent?: string;
+      title?: string;
+    } = {},
+  ): HTMLElementTagNameMap[K] {
+    const element = document.createElement(tag);
+    if (options.styles) Object.assign(element.style, options.styles);
+    if (options.textContent) element.textContent = options.textContent;
+    if (options.title) element.title = options.title;
+    return element;
+  }
+  
+  // append header element for title
+  const title = createElement('h1', {
+    textContent: "My Sketchpad App",
+  });
+  app.appendChild(title);
+  
+  // create and append canvas element
+  const canvas = createElement('canvas', {
+    styles: {
+      width: '256px',
+      height: '256px',
+    },
+  });
+  canvas.id = "myCanvas";  // add an ID for styling in CSS
+  app.appendChild(canvas);
